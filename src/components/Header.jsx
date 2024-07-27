ccef766dd8df127598946ea8f77659e728cf622b
Faysal-Maxamed <pheymoha17@gmail.com>
2024-07-27 11:12:04 -0700

    Header added profile

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,16 @@
 import React, { useState, useEffect, useRef } from 'react';
 import { Link, useNavigate, useLocation } from 'react-router-dom';
+import { FaUserCircle, FaSignOutAlt } from 'react-icons/fa'; // Import React Icons
 
 const Header = ({ user, onLogout }) => {
   const [menuOpen, setMenuOpen] = useState(false);
   const [coursesOpen, setCoursesOpen] = useState(false);
-  const [adminMenuOpen, setAdminMenuOpen] = useState(false); // New state for admin menu
+  const [profileOpen, setProfileOpen] = useState(false); // New state for profile menu
   const navigate = useNavigate();
   const location = useLocation();
   const menuRef = useRef(null);
   const coursesRef = useRef(null);
-  const adminMenuRef = useRef(null); // Ref for admin menu
+  const profileRef = useRef(null); // Ref for profile menu
 
   const handleLogout = () => {
     if (window.confirm('Are you sure you want to log out?')) {
@@ -26,7 +27,9 @@ const Header = ({ user, onLogout }) => {
     setCoursesOpen(!coursesOpen);
   };
 
- 
+  const toggleProfile = () => {
+    setProfileOpen(!profileOpen);
+  };
 
   const handleClickOutside = (event) => {
     if (menuRef.current && !menuRef.current.contains(event.target)) {
@@ -35,8 +38,8 @@ const Header = ({ user, onLogout }) => {
     if (coursesRef.current && !coursesRef.current.contains(event.target)) {
       setCoursesOpen(false);
     }
-    if (adminMenuRef.current && !adminMenuRef.current.contains(event.target)) { // Handle admin menu click outside
-      setAdminMenuOpen(false);
+    if (profileRef.current && !profileRef.current.contains(event.target)) {
+      setProfileOpen(false);
     }
   };
 
@@ -48,7 +51,7 @@ const Header = ({ user, onLogout }) => {
   }, []);
 
   const getLinkClass = (path) => {
-    return location.pathname === path ? 'text-yellow-500' : '';
+    return location.pathname === path ? 'text-blue-500' : '';
   };
 
   const navigateToForm2 = () => {
@@ -63,14 +66,6 @@ const Header = ({ user, onLogout }) => {
     navigate('/form4');
   };
 
-  const navigateToRegisterAdmin = () => {
-    navigate('/registeradmin');
-  };
-
-  const navigateToAdmin = () => {
-    navigate('/admin');
-  };
-
   return (
     <header className="bg-white text-gray-800 p-4 flex justify-between items-center shadow-md relative z-20">
       <Link to="/home" className="text-2xl font-bold flex items-center">
@@ -80,7 +75,8 @@ const Header = ({ user, onLogout }) => {
         </h1>
       </Link>
       <nav className="space-x-4 flex items-center">
-        <div className="hidden md:flex space-x-8">
+        {/* Desktop Menu */}
+        <div className="hidden md:flex space-x-8 items-center">
           <Link to="/home" className={`hover:text-green-500 ${getLinkClass('/home')}`}>
             Home
           </Link>
@@ -109,40 +105,38 @@ const Header = ({ user, onLogout }) => {
               </div>
             )}
           </div>
-        
+
           <Link to="/contact-us" className={`hover:text-green-500 ${getLinkClass('/contact-us')}`}>
             Contact
           </Link>
-          <button
-            onClick={handleLogout}
-            className="bg-gradient-to-r from-green-400 to-blue-500 text-white px-4 py-2 rounded-md hidden md:block"
-          >
-            Logout
-          </button>
+
+          <div className="relative" ref={profileRef}>
+            <button onClick={toggleProfile} className="hover:text-green-500">
+              <h1 className="bg-gradient-to-r from-green-400 to-blue-500 text-white w-full px-4 py-2 rounded-md">Profile</h1>
+            </button>
+            {profileOpen && (
+              <div className="absolute right-0 top-10 w-48 bg-white border border-gray-200 divide-y divide-gray-200 rounded-md shadow-lg z-30">
+                <div className="flex items-center p-2 text-lg font-medium">
+                  <FaUserCircle size={24} />
+                  <div className="ml-2">{user.email}</div>
+                </div>
+                <button
+                  onClick={handleLogout}
+                  className="block w-full text-left p-2 text-lg font-medium text-red-500 transition duration-300 hover:bg-red-50"
+                >
+                  <FaSignOutAlt className="inline-block mr-2" /> Logout
+                </button>
+              </div>
+            )}
+          </div>
         </div>
 
+        {/* Mobile Menu */}
         <div className="md:hidden">
-          <button
-            onClick={toggleMenu}
-            className="block text-gray-800 hover:text-gray-900 focus:outline-none"
-          >
-            <svg
-              className="h-6 w-6 text-gray-800" // Ensure the color is visible
-              viewBox="0 0 24 24"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                className={menuOpen ? 'hidden' : 'block'}
-                d="M4 6h16M4 12h16M4 18h16"
-                stroke="currentColor" // Use currentColor for consistent color
-                strokeWidth="2"
-              />
-              <path
-                className={menuOpen ? 'block' : 'hidden'}
-                d="M6 18L18 6M6 6l12 12"
-                stroke="currentColor" // Use currentColor for consistent color
-                strokeWidth="2"
-              />
+          <button onClick={toggleMenu} className="block text-gray-800 hover:text-gray-900 focus:outline-none">
+            <svg className="h-6 w-6 text-gray-800" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+              <path className={menuOpen ? 'hidden' : 'block'} d="M4 6h16M4 12h16M4 18h16" stroke="currentColor" strokeWidth="2" />
+              <path className={menuOpen ? 'block' : 'hidden'} d="M6 18L18 6M6 6l12 12" stroke="currentColor" strokeWidth="2" />
             </svg>
           </button>
           {menuOpen && (
@@ -177,22 +171,35 @@ const Header = ({ user, onLogout }) => {
                       </div>
                     )}
                   </div>
-              
+
                   <Link to="/contact-us" className={`block px-4 py-2 ${getLinkClass('/contact-us')}`}>
                     Contact
                   </Link>
-                  <button
-                    onClick={handleLogout}
-                    className="bg-gradient-to-r from-green-400 to-blue-500 text-white w-full px-4 py-2 rounded-md"
-                  >
-                    Logout
-                  </button>
+
+                  <div className="relative" ref={profileRef}>
+                    <button onClick={toggleProfile} className="bg-gradient-to-r from-green-400 to-blue-500 text-white w-full px-4 py-2 rounded-md">
+                      Profile
+                    </button>
+                    {profileOpen && (
+                      <div className="absolute right-0 top-10 w-full bg-white border border-gray-200 divide-y divide-gray-200 rounded-md shadow-lg z-30">
+                        <div className="flex items-center p-2 text-lg font-medium">
+                          <FaUserCircle size={24} />
+                          <div className="ml-2">{user.email}</div>
+                        </div>
+                        <button
+                          onClick={handleLogout}
+                          className="block w-full text-left p-2 text-lg font-medium text-red-500 transition duration-300 hover:bg-red-50"
+                        >
+                          <FaSignOutAlt className="inline-block mr-2" /> Logout
+                        </button>
+                      </div>
+                    )}
+                  </div>
                 </>
               )}
             </div>
           )}
         </div>
-
       </nav>
     </header>
   );
