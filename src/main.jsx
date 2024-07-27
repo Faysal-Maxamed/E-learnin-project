93c05110002067e95a4344ea859aa29d736f0bbb
Faysal-Maxamed <pheymoha17@gmail.com>
2024-07-27 05:02:52 -0700

    Commit

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import App from './App';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <React.StrictMode basename="/E-learnin-project">
     <App />
   </React.StrictMode>,
 );
